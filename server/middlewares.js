831a89ec226fc1ff895f807eae1165f0f7d4eda7
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2025-03-21 15:20:33 +0100

    add style for error 404 and delete middlewares

--- server/middlewares.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    const db = req.app.db;
-    const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(',')[0].trim();
-    
-    next();
-};
