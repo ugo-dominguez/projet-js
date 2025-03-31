ba88451461819f9dabe9d010338722486bfcb325
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2025-03-31 08:17:32 +0200

    add better paths for images

--- public/lib/config.js
+++ public/lib/config.js
@@ -1,10 +1,10 @@
 export const ENDPOINT = 'http://localhost:3000';
 export const ITEMS_PER_PAGE = 10;
-export const MONSTERS_THUMB_PATH = './assets/images/monsters_thumb/';
-export const MONSTERS_MODELS_PATH = './assets/images/monsters_models/';
-export const FAMILIES_ICONS_PATH = './assets/images/families_icons/';
+export const MONSTERS_THUMB_PATH = './assets/images/monsters_thumb';
+export const MONSTERS_MODELS_PATH = './assets/images/monsters_models';
+export const FAMILIES_ICONS_PATH = './assets/images/families_icons';
 export const STATS_ICONS_PATH = './assets/images/stats_icons';
-export const BOXES_IMG_PATH = './assets/images/boxes/';
+export const BOXES_IMG_PATH = './assets/images/boxes';
 export const ACCESSORY_IMG_PATH = './assets/images/accessory.png';
 export const MAX_IN_PARTY = 4;
 export const STATS_MAP = {
